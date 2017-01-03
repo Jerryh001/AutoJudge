8147a8a66b560cec0f45a6cc34ee8f14dd8c2ceb
Jerryh001 <jerryh001@hotmail.com>
2017-01-03 20:46:02 +0800

    移除debug訊息

--- AutoJudge/Compile.cpp
+++ AutoJudge/Compile.cpp
@@ -6,9 +6,9 @@ using namespace std;
 int Compile(const string& filename)
 {
 	string GppPath = Config::GetData("compiler");
-	string CompileOption = R"( -Wall -O2 -o .\AJ_temp\a.exe >nul )";
+	string CompileOption = R"( -Wall -O2 -o .\AJ_temp\a.exe >nul)";
 	string CompileCommend = R"(cmd /C "set "PATH=)"+ GppPath +R"(;%PATH%" && g++ ".\)" + filename + R"(" )" + CompileOption + R"(")";
-	cout << CompileCommend << endl;
+	//cout << CompileCommend << endl;
 	return system(CompileCommend.c_str());
 	
 }
